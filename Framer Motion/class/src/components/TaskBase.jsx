b3e8009cc1f03d313220b37cb0bb357008f95c14
whoami1111121 <udud0099@gmail.com>
2026-02-26 09:51:38 +0545

    update

--- Framer Motion/class/src/components/TaskBase.jsx
+++ Framer Motion/class/src/components/TaskBase.jsx
@@ -2,7 +2,11 @@ import { motion } from "motion/react";
 const TaskBase = () => {
   return (
     <div>
-      <motion.div className="p-8" animate={{ x: 360 }}>
+      <motion.div
+        className="p-8"
+        initial={{ scale: 0 }}
+        animate={{ x: 360, transition: { duration: 2 }, scale: 1 }}
+      >
         <h1>Task Base js</h1>
       </motion.div>
     </div>
