684c5647ac51d505a18b00f5165a20cbdbd28197
whoami1111121 <udud0099@gmail.com>
2026-02-26 09:47:50 +0545

    Framer Motion class start

---
+++ Framer Motion/basejsfm - Copy (2)/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import TaskBase from "./components/TaskBase";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white w-full min-h-screen text-2xl">
+        <TaskBase />
+      </div>
+    </>
+  );
+}
+
+export default App;
